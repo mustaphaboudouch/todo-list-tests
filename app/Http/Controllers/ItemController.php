0298154e03488a624ac1a762ab657231cef90e66
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-04-25 17:16:58 +0200

    feat: add item validation

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -34,16 +35,39 @@ class ItemController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        $todoList = $request->user()->todoList;
+
+        // user can only create 10 items
+        if ($todoList->items()->count() === 10) {
+            return redirect()->route('items.index')
+                ->with('status', 'You can only create 10 items.');
+        }
+
+        // validate item creating date
+        $item = $todoList->items()->orderBy('created_at', 'DESC')->first();
+        $now = Carbon::now();
+        $diff = $now->diffInMinutes($item->created_at);
+        $rest = 30 - $diff;
+
+        if ($diff < 30) {
+            return redirect()->route('items.index')
+                ->with('status', 'Wait for ' .  $rest . ' min.');
+        }
+
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'content' => ['required', 'string', 'max:1000'],
         ]);
 
-        $todoList = $request->user()->todoList;
         $todoList->items()->create([
             'name' => $request->name,
             'content' => $request->content,
         ]);
+
+        // warn user that remains only 2 items to create
+        if ($todoList->items()->count() === 8) {
+            // send email to user
+        }
 
         return redirect()->route('items.index')
             ->with('status', 'The item has been added successfully.');
