5f72020c81b491e3ecc70be64c9102645135e392
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-04-25 15:27:46 +0200

    feat: setup laravel breeze auth

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,9 +18,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'is_adult' => fake()->boolean(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
