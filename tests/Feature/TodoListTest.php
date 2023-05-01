0109294b5d8cc96cc80ccdc2aa92acfe229d7de4
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-05-01 19:02:12 +0200

    test: add todolist tests

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_todolist_is_created_on_user_register(): void
+    {
+        $response = $this->post('/register', [
+            'firstname' => 'Mustapha',
+            'lastname' => 'Boudouch',
+            'email' => 'test@example.com',
+            'password' => 'Abcd12345',
+            'is_adult' => true,
+        ]);
+
+        $user = User::where('email', 'test@example.com')->first();
+
+        $this->assertTrue(!!$user->todoList);
+    }
+
+    public function test_user_has_one_todolist(): void
+    {
+        $response = $this->post('/register', [
+            'firstname' => 'Mustapha',
+            'lastname' => 'Boudouch',
+            'email' => 'test@example.com',
+            'password' => 'Abcd12345',
+            'is_adult' => true,
+        ]);
+
+        $user = User::where('email', 'test@example.com')->first();
+
+        $this->assertDatabaseCount('todo_lists', 1);
+    }
+}
